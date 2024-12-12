54c650918841481a17639351a88de9b7cdb24b19
ayaamar2001 <129336185+ayaamar200@users.noreply.github.com>
2024-12-12 21:14:05 +0200

    first commit

---
+++ js/index.js
@@ -0,0 +1,65 @@
+// ! ******************** Global Variables ********************
+
+// Input
+let searchInput = document.getElementById("searchInput");
+let searchBtn = document.getElementById("searchBtn");
+
+// variables
+let allRecipes = [];
+// ! ******************** Global Functions ********************
+searchInput.addEventListener("input",function(){
+  let searchValue = searchInput.value;
+  console.log(searchValue);
+  getRecipes(searchValue);
+})
+
+searchBtn.addEventListener("click",function(){
+  let searchValue = searchInput.value;
+  console.log(searchValue);
+  getRecipes(searchValue);
+})
+
+
+// AJAX Function
+
+function getRecipes(meal) {
+  let http = new XMLHttpRequest();
+  http.open("GET", `https://forkify-api.herokuapp.com/api/search?q=${meal}`);
+  http.send();
+  http.responseType = "json";
+  http.addEventListener("load", function () {
+    if (http.status >= 200 && http.status < 300) {
+      allRecipes = http.response.recipes;
+      displayRecipes();
+      // console.log(allRecipes);
+    }
+  });
+  http.addEventListener("error", function () {
+    console.log("Error");
+  });
+}
+
+
+
+function displayRecipes() {
+  let recipesContainer = "";
+  for (let i = 0; i < allRecipes.length; i++) {
+    recipesContainer += `
+    <div class="col-md-3">
+        <div class="card">
+          <img class="card-img-top" src="${allRecipes[i].image_url}" alt="${
+      allRecipes[i].title
+    }"  height="200" />
+          <div class="card-body">
+            <h3 class="card-title h5">${allRecipes[i].title
+              .split(" ", 3)
+              .join(" ")}</h3>
+          </div>
+        </div>
+              
+    </div>
+    `;
+  }
+
+  document.getElementById("recipesData").innerHTML = recipesContainer;
+}
